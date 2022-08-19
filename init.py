0101a30f9b9bd066e8efda8a484645b34b10a84b
B. Ionut <61597084+bionut15@users.noreply.github.com>
2022-08-19 12:34:02 +0300

    Create init.py

---
+++ init.py
@@ -0,0 +1,24 @@
+#made by ionut b
+
+from PyQt5.QtCore import QSize, Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow 
+from PyQt5 import QtGui
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        
+        #Main window configuration
+       
+        self.setWindowFlag(Qt.FramelessWindowHint)
+        self.setFixedSize(QSize(1600, 900))
+       # self.setAttribute(Qt.WA_TranslucentBackground)
+   
+       
+
+if __name__ == "__main__":
+    import sys
+    app = QApplication(sys.argv)
+    w = MainWindow()
+    w.show()
+    sys.exit(app.exec_())
