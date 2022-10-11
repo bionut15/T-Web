14c89fc38b1c6e0bd2bda9e2ccf870c1ca1bc955
B. Ionut <61597084+bionut15@users.noreply.github.com>
2022-10-11 14:29:53 +0300

    Now resizable and grameless

--- init.py
+++ init.py
@@ -1,179 +1,9 @@
 #made by ionut b
-
-from PyQt5.QtCore import QSize, Qt, QRectF, QPoint
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QFrame, QSizeGrip 
-from PyQt5 import QtGui, QtWidgets, QtCore
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import QPoint
 from PyQt5.QtGui import QPainterPath, QRegion
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QFrame, QSizeGrip, QGridLayout 
 
-class MainWindow(QMainWindow):
-    _gripSize = 8
-    def __init__(self):
-        super(MainWindow, self).__init__()
-        #Main window configuration
-        self.setWindowFlag(Qt.FramelessWindowHint)
-        self.setAttribute(Qt.WA_TranslucentBackground)
-        self.setMinimumSize(900, 750)
-        self.resize(1600, 900)
-        self.mainframe=QFrame(self)
-        self.mainframe.setStyleSheet(
-        """
-            background: rgba(70, 70, 70, 0.58);
-            border: 1px solid #BBBBBB;
-            border-radius: 24px;
-                """)
-        self.setCentralWidget(self.mainframe)
-        self.ui_componets()
-        self.sideGrips = [
-            SideGrip(self, QtCore.Qt.LeftEdge), 
-            SideGrip(self, QtCore.Qt.TopEdge), 
-            SideGrip(self, QtCore.Qt.RightEdge), 
-            SideGrip(self, QtCore.Qt.BottomEdge), 
-        ]
-        # corner grips should be "on top" of everything, otherwise the side grips
-        # will take precedence on mouse events, so we are adding them *after*;
-        # alternatively, widget.raise_() can be used
-        self.cornerGrips = [QtWidgets.QSizeGrip(self) for i in range(4)]
-
- 
-    def resizeEvent(self, event):
-        QMainWindow.resizeEvent(self, event)
-        rect = self.rect()
-        # top left grip doesn't need to be moved...
-        # top right
-        self.grips[1].move(rect.right() - self.gripSize, 0)
-        # bottom right
-        self.grips[2].move( rect.right() - self.gripSize, rect.bottom() - self.gripSize)
-        # bottom left
-        self.grips[3].move(0, rect.bottom() - self.gripSize)
-
-    @property
-    def gripSize(self):
-        return self._gripSize
-
-    def setGripSize(self, size):
-        if size == self._gripSize:
-            return
-        self._gripSize = max(2, size)
-        self.updateGrips()
-
-    def updateGrips(self):
-        self.setContentsMargins(*[self.gripSize] * 4)
-
-        outRect = self.rect()
-        # an "inner" rect used for reference to set the geometries of size grips
-        inRect = outRect.adjusted(self.gripSize, self.gripSize,
-            -self.gripSize, -self.gripSize)
-
-        # top left
-        self.cornerGrips[0].setGeometry(
-            QtCore.QRect(outRect.topLeft(), inRect.topLeft()))
-        # top right
-        self.cornerGrips[1].setGeometry(
-            QtCore.QRect(outRect.topRight(), inRect.topRight()).normalized())
-        # bottom right
-        self.cornerGrips[2].setGeometry(
-            QtCore.QRect(inRect.bottomRight(), outRect.bottomRight()))
-        # bottom left
-        self.cornerGrips[3].setGeometry(
-            QtCore.QRect(outRect.bottomLeft(), inRect.bottomLeft()).normalized())
-
-        # left edge
-        self.sideGrips[0].setGeometry(
-            0, inRect.top(), self.gripSize, inRect.height())
-        # top edge
-        self.sideGrips[1].setGeometry(
-            inRect.left(), 0, inRect.width(), self.gripSize)
-        # right edge
-        self.sideGrips[2].setGeometry(
-            inRect.left() + inRect.width(), 
-            inRect.top(), self.gripSize, inRect.height())
-        # bottom edge
-        self.sideGrips[3].setGeometry(
-            self.gripSize, inRect.top() + inRect.height(), 
-            inRect.width(), self.gripSize)
-
-    def resizeEvent(self, event):
-        QtWidgets.QMainWindow.resizeEvent(self, event)
-        self.updateGrips()
-    def ui_componets(self):
-  #Border settings
-            #close button
-        cbutton = QPushButton('', self)
-        cbutton.clicked.connect(QApplication.instance().quit)
-        cbutton.resize(20,20)
-        cbutton.move(1560,18)   
-        cbutton.setStyleSheet(
-        """
-            background: #FF2626;
-            border-radius: 10px;
-                """)
-            #maximize button
-        mbutton = QPushButton('', self)
-        mbutton.clicked.connect(self.maximize)
-        mbutton.resize(20,20)
-        mbutton.move(1530,18) 
-        mbutton.setStyleSheet(
-        """
-            background: #34FF6D;
-            border-radius : 10px;
-                """)
-            #minimize button
-        nbutton = QPushButton('', self)
-        nbutton.clicked.connect(self.minimize)
-        nbutton.resize(20,20)
-        nbutton.move(1500,18)        
-        nbutton.setStyleSheet(
-        """
-            background: #F0FF40;
-            border-radius : 10px; 
-                """)
-    #window area
-        frame =QFrame(self)
-        frame.setFrameShape(QFrame.StyledPanel)
-        frame.setLineWidth(6)
-        frame.resize(1300,864)
-        frame.move(18,18)
-        frame.setStyleSheet("""
-            border: 1px solid #BBBBBB;
-            border-radius: 16px;     
-            background: #1B1D1E;
-                """)
-    #widget area
-        widget =QFrame(self)
-        widget.setFrameShape(QFrame.StyledPanel)
-        widget.setLineWidth(6)
-        widget.move(1332, 50)
-        widget.resize(250, 832)
-        widget.setStyleSheet("""
-            background: rgba(255, 255, 255, 0.21);
-            border-radius: 16px;
-                """)
-    def minimize(self):
-        self.showMinimized()
-    def maximize(self):
-        state = int(self.windowState())
-        if state == 0:
-            self.showMaximized()
-        else:
-             self.showNormal()
-    def resizeEvent(self, event):
-        path = QPainterPath()
-        print(self.rect())
-        path.addRoundedRect(QRectF(self.rect()),25, 25,Qt.AbsoluteSize)
-        reg = QRegion(path.toFillPolygon().toPolygon())
-        self.setMask(reg)
-    def mousePressEvent(self, event):
-        self.oldPosition = event.globalPos()
-    def mouseMoveEvent(self, event):
-        delta = QPoint(event.globalPos() - self.oldPosition)
-        self.move(self.x() + delta.x(),self.y() + delta.y())
-        self.oldPosition = event.globalPos()
-    def mouseDoubleClickEvent(self, event):
-        state = int(self.windowState())
-        if state == 0:
-            self.showMaximized()
-        else:
-             self.showNormal()
 class SideGrip(QtWidgets.QWidget):
     def __init__(self, parent, edge):
         QtWidgets.QWidget.__init__(self, parent)
@@ -190,7 +20,6 @@ class SideGrip(QtWidgets.QWidget):
             self.setCursor(QtCore.Qt.SizeVerCursor)
             self.resizeFunc = self.resizeBottom
         self.mousePos = None
-
     def resizeLeft(self, delta):
         window = self.window()
         width = max(window.minimumWidth(), window.width() - delta.x())
@@ -226,9 +55,142 @@ class SideGrip(QtWidgets.QWidget):
 
     def mouseReleaseEvent(self, event):
         self.mousePos = None
-if __name__ == "__main__":
-    import sys
-    app = QApplication(sys.argv)
-    w = MainWindow()
-    w.show()
-    sys.exit(app.exec_())
+
+
+class Main(QtWidgets.QMainWindow):
+    _gripSize = 8
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+
+        self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+       # self.setAttribute(Qt.WA_TranslucentBackground)
+        self.sideGrips = [
+            SideGrip(self, QtCore.Qt.LeftEdge), 
+            SideGrip(self, QtCore.Qt.TopEdge), 
+            SideGrip(self, QtCore.Qt.RightEdge), 
+            SideGrip(self, QtCore.Qt.BottomEdge), 
+        ]
+        # corner grips should be "on top" of everything, otherwise the side grips
+        # will take precedence on mouse events, so we are adding them *after*;
+        # alternatively, widget.raise_() can be used
+        self.cornerGrips = [QtWidgets.QSizeGrip(self) for i in range(4)]
+        self.resize(1600, 900)
+       # self.setMinimumSize(800, 550)
+        self.mainframe=QFrame(self)
+        self.mainframe.setStyleSheet(
+        """
+            background: rgba(70, 70, 70, 0.58);
+            border: 1px solid #BBBBBB;
+            border-radius: 24px;
+                """)
+        self.setCentralWidget(self.mainframe)
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground, True)
+        #main_frame
+        frame =QtWidgets.QFrame(self)
+        frame.setFrameShape(QFrame.StyledPanel)
+        frame.setLineWidth(0)
+        frame.setStyleSheet("""
+            border: 1px solid #BBBBBB;
+            border-radius: 16px;     
+            background: #1B1D1E;
+                """)
+        self.setCentralWidget(frame)
+        grid_1 = QGridLayout()
+        grid_1.setHorizontalSpacing(10)
+        grid_1.setVerticalSpacing(10)
+        frame.setLayout(grid_1)
+        #window_1
+        window_1 = QFrame(self)
+        grid_1.addWidget(window_1,0,1,1,1)
+        window_1.setStyleSheet("""
+            border: 1px solid #BBBBBB;
+            border-radius: 16px;     
+            background: #1B1D1E;
+                """)
+        #widgets_area
+        widgets_area = QFrame(self)
+        widgets_area.setFixedWidth(256)
+        widgets_area.setStyleSheet("""
+            background: rgba(255, 255, 255, 0.21);
+            border-radius: 16px;
+        """)
+        grid_1.addWidget(widgets_area,0,0,1,1)
+    @property
+    def gripSize(self):
+        return self._gripSize
+    def setGripSize(self, size):
+        if size == self._gripSize:
+            return
+        self._gripSize = max(2, size)
+        self.updateGrips()
+    def updateGrips(self):
+        self.setContentsMargins(*[self.gripSize] * 4)
+
+        outRect = self.rect()
+        # an "inner" rect used for reference to set the geometries of size grips
+        inRect = outRect.adjusted(self.gripSize, self.gripSize,
+            -self.gripSize, -self.gripSize)
+
+        # top left
+        self.cornerGrips[0].setGeometry(
+            QtCore.QRect(outRect.topLeft(), inRect.topLeft()))
+        self.cornerGrips[0].setStyleSheet("""
+                background-color: transparent; 
+        """)
+        # top right
+        self.cornerGrips[1].setGeometry(
+            QtCore.QRect(outRect.topRight(), inRect.topRight()).normalized())
+        self.cornerGrips[1].setStyleSheet("""
+                background-color: transparent; 
+        """)
+        # bottom right
+        self.cornerGrips[2].setGeometry(
+            QtCore.QRect(inRect.bottomRight(), outRect.bottomRight()))
+        self.cornerGrips[2].setStyleSheet("""
+                background-color: transparent; 
+        """)
+        # bottom left
+        self.cornerGrips[3].setGeometry(
+            QtCore.QRect(outRect.bottomLeft(), inRect.bottomLeft()).normalized())
+        self.cornerGrips[3].setStyleSheet("""
+                background-color: transparent; 
+        """)
+        # left edge
+        self.sideGrips[0].setGeometry(
+            0, inRect.top(), self.gripSize, inRect.height())
+        self.sideGrips[3].setStyleSheet("""
+                background-color: transparent; 
+        """)
+        # top edge
+        self.sideGrips[1].setGeometry(
+            inRect.left(), 0, inRect.width(), self.gripSize)
+        self.sideGrips[1].setStyleSheet("""
+                background-color: transparent; 
+        """)
+        # right edge
+        self.sideGrips[2].setGeometry(
+            inRect.left() + inRect.width(), 
+            inRect.top(), self.gripSize, inRect.height())
+        # bottom edge
+        self.sideGrips[3].setGeometry(
+            self.gripSize, inRect.top() + inRect.height(), 
+            inRect.width(), self.gripSize)
+    def resizeEvent(self, event):
+        QtWidgets.QMainWindow.resizeEvent(self, event)
+        self.updateGrips()
+    def mousePressEvent(self, event):
+        self.oldPosition = event.globalPos()
+    def mouseMoveEvent(self, event):
+        delta = QPoint(event.globalPos() - self.oldPosition)
+        self.move(self.x() + delta.x(),self.y() + delta.y())
+        self.oldPosition = event.globalPos()
+    def mouseDoubleClickEvent(self, event):
+        state = int(self.windowState())
+        if state == 0:
+            self.showMaximized()
+        else:
+            self.showNormal()
+app = QtWidgets.QApplication([])
+m = Main()
+m.show()
+app.exec_()
