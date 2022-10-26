0b408d71677a75a279593796fb9cebf909690f98
B. Ionut <61597084+bionut15@users.noreply.github.com>
2022-10-26 15:41:23 +0300

    so i added a status bar#

--- init.py
+++ init.py
@@ -2,7 +2,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import *
 from PyQt5.QtGui import QPainterPath, QRegion
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QFrame, QSizeGrip, QGridLayout, QVBoxLayout, QStatusBar, QHBoxLayout, QAction
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QFrame, QSizeGrip, QGridLayout, QVBoxLayout, QStatusBar, QHBoxLayout, QAction, QLineEdit
 from PyQt5.QtWebEngineWidgets import *
 from PyQt5.QtPrintSupport import *
 import os
@@ -97,12 +97,13 @@ class Main(QtWidgets.QMainWindow):
         #window_1
         self.browser = QWebEngineView()
         self.browser.setUrl(QUrl("https://google.com"))
-
         grid_1.addWidget(self.browser,0,1,3,3)
         self.browser.setStyleSheet("""
+           
             border: 1px solid #BBBBBB;
-            border-radius: 16px;     
-            background: #1B1D1E;
+            border-radius: 16em;     
+            background: #1B1D34
+            ;
                 """)
         #widgets_area
    #Grid second
@@ -112,13 +113,46 @@ class Main(QtWidgets.QMainWindow):
         grid_2.setSpacing(10)
     #Grid 3
         grid_3 = QHBoxLayout()
-        grid_2.addLayout(grid_3)
+
         back_btn = QPushButton("<", self)
+      #  back_btn.triggered.connect(self.browser.back)
+        back_btn.setStyleSheet("""color:white;
+                               height:2em;
+                                margin:2em;width:2em; border-radius: 1em;""")
         forward_btn = QPushButton(">",self)
-        reload_btn = QPushButton("0",self)
+        forward_btn.setStyleSheet("""color:white;
+                               height:2em;
+                                margin:2em;width:2em; border-radius: 1em;""")
+        reload_btn = QPushButton("O",self)
+        reload_btn.setStyleSheet("""color:white; height:2em;
+                                margin:2em;width:2em; border-radius: 1em;""")
+        self.searchBar = QLineEdit()
+        self.searchBar.setStyleSheet(
+                """
+                height:2em;
+                width:3em;
+                color: white;
+                border-radius: 1em;
+                """
+                )
+        self.searchBar.setFixedWidth(250)
+        self.searchBar.returnPressed.connect(self.loadUrl)
+        grid_2.addWidget(self.searchBar)
+        self.browser.urlChanged.connect(self.updateUrl)
+        grid_2.addLayout(grid_3)
         grid_3.addWidget(back_btn)
         grid_3.addWidget(reload_btn)
         grid_3.addWidget(forward_btn)
+        #method to load the required url
+    def loadUrl(self):
+        #fetching entered url from searchBar
+        url = self.searchBar.text()
+        #loading url
+        self.browser.setUrl(QUrl(url))
+    #method to update the url
+    def updateUrl(self, url):
+        #changing the content(text) of searchBar
+        self.searchBar.setText(url.toString())
     def resizeEvent(self, event):
         QtWidgets.QMainWindow.resizeEvent(self, event)
         self.updateGrips()
